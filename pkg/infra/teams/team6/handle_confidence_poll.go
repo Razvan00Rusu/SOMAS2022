77fdf1f562ac3677e444f8fde77ba5b3dba77c9f
Timothy Moores <info@tmoor.es>
2022-12-01 16:01:32 +0000

    add vonc logic

--- pkg/infra/teams/team6/handle_confidence_poll.go
+++ pkg/infra/teams/team6/handle_confidence_poll.go
@@ -3,16 +3,38 @@ package team6
 import (
 	"infra/game/agent"
 	"infra/game/decision"
-	"math/rand"
 )
 
-func (r *Team6Agent) HandleConfidencePoll(_ agent.BaseAgent) decision.Intent {
-	switch rand.Intn(3) {
-	case 0:
-		return decision.Abstain
-	case 1:
-		return decision.Negative
-	default:
+// proportion of agents we would rather have as leader to vote yes
+const VONC_THRESHOLD = 0.5
+
+func (r *Team6Agent) HandleConfidencePoll(
+	base agent.BaseAgent,
+) decision.Intent {
+	view := base.View()
+	leader := view.CurrentLeader()
+	leaderScore := getVotingScore(r.getOpinion(leader))
+	numBetter := 0
+	numEq := 0
+	agents := view.AgentState()
+	it := agents.Iterator()
+	for !it.Done() {
+		id, _, ok := it.Next()
+		if ok {
+			score := getVotingScore(r.getOpinion(id))
+			if score > leaderScore {
+				numBetter++
+			}
+			if score >= leaderScore {
+				numEq++
+			}
+		}
+	}
+	if float64(numBetter)/float64(agents.Len()) >= VONC_THRESHOLD {
 		return decision.Positive
 	}
+	if float64(numEq)/float64(agents.Len()) >= VONC_THRESHOLD {
+		return decision.Abstain
+	}
+	return decision.Negative
 }
